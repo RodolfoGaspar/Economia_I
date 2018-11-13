99906b9fe73691bb8af9be5271185874bc460954
Rodolfo Gaspar <dolfogaspar@gmail.com>
2018-11-12 23:27:37 -0200

    criado algoritimo para resolução da exercicio 3 e 4 da Atividade para Avaliação da Semana 3

--- Economia/Program.cs
+++ Economia/Program.cs
@@ -10,6 +10,39 @@ namespace Economia
 	{
 		static void Main(string[] args)
 		{
+
+			Console.WriteLine("Economia - Atividade para Avaliação - Semana 3\n");
+
+			var Q = 10; // Quantidade
+			double C = 0; // Custo
+
+			Console.WriteLine($"Quantidade de produtos (Q): {Q}");
+			Console.WriteLine("Fórmula Custo (C): C(Q) = 400+3Q²");
+
+			C = 400 + 3 * Math.Pow(Q, 2);
+
+			Console.WriteLine($"Custo = {C}");
+
+			Q = 20;
+			double P = 100;
+
+			Console.WriteLine($"Quantidade de produtos (Q): {Q}");
+			Console.WriteLine($"Preço Unitário (P): {P}");
+			Console.WriteLine("Fórmula Custo (C): C(Q) = 400+3Q²");
+
+			C = 400 + 3 * Math.Pow(Q, 2);
+
+			Console.WriteLine($"Custo = {C}");
+
+			Console.WriteLine("Fórmula Lucro (L): L=RT-CT");
+			Console.WriteLine("Fórmula Receita Total (L): RT=Q*P");
+
+			var L = P * Q - C;
+
+			Console.WriteLine($"Lucro = {L}");
+
+			Console.ReadKey();
+
 		}
 	}
 }
